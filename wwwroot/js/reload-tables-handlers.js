962075bb5d1f66d763d255a621e2d20033c04893
Viet Doan <viet18725@gmail.com>
2025-11-26 17:53:14 +0700

    them file ProgramDetail.cshtml

--- wwwroot/js/reload-tables-handlers.js
+++
@@ -1,8 +0,0 @@
-// Function to reset table and reload data
-function resetAndLoad() {
-    $("tbody").empty();
-    sttCounter = 0;
-    currentPage = 0;
-    totalPages = 1; // Reset to trigger load
-    loadMoreCourses();
-}
