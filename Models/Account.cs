6af288f0668bdf7267b86d9119f0026bf3ca92c0
Viet Doan <viet18725@gmail.com>
2025-12-01 10:09:39 +0700

    cap nhat cac properties trong Account.cs

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,6 +1,47 @@
-﻿namespace SIMS.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIMS.Models
 {
+    [Table("Account")]
     public class Account
     {
+        [Key]
+        [Column("account_id")]
+        public int AccountId { get; set; }
+
+
+        [Column("person_id")]
+        public int? PersonId { get; set; }
+
+
+        [Column("user_name")]
+        public string? UserName { get; set; }
+
+
+        [Column("password")]
+        public string? Password { get; set; }
+
+
+        [Column("role_id")]
+        public int? RoleId { get; set; }
+
+        [Column("created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [Column("is_deleted")]
+        public bool? IsDeleted { get; set; }
+
+        [ForeignKey(nameof(PersonId))]
+        public virtual Person Person { get; set; } = null!;
+
+        [ForeignKey(nameof(RoleId))]
+        public virtual Role Role { get; set; } = null!;
+
+
     }
 }
